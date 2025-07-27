753b019a8da4a22cd53a884c07b9aaa2a0c9418a
fawwzrf <fawwaz.rafi@mhs.unsoed.ac.id>
2025-07-27 22:24:04 +0700

    Refactor Lokasi dan LokasiModel

--- app/Modules/Master/Controllers/Lokasi.php
+++ app/Modules/Master/Controllers/Lokasi.php
@@ -5,8 +5,7 @@ namespace App\Modules\Master\Controllers;
 use App\Http\Controllers\MyController;
 use App\Modules\App\Models\DbModel;
 use App\Modules\Master\Models\LokasiModel;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\DB;
+
 
 class Lokasi extends MyController
 {
@@ -56,36 +55,6 @@ class Lokasi extends MyController
         }
         return $this->renderView($this->template . 'form_modal', $d);
     }
-
-    /**
-     * Generate lokasi_id baru berdasarkan tipe dan parent
-     */
-    private function _generateLokasiId($tipe, $parentId = null)
-    {
-        $prefix = $parentId ? $parentId . '.' : '';
-        $query = "SELECT MAX(lokasi_id) as last_id FROM mst_lokasi WHERE tipe_lokasi = ? AND deleted_st = 0";
-        $params = [$tipe];
-
-        if ($parentId) {
-            $query .= " AND parent_lokasi_id = ?";
-            $params[] = $parentId;
-        } else {
-            $query .= " AND parent_lokasi_id IS NULL";
-        }
-
-        $lastData = DbModel::rawData('row_array', $query, $params);
-        $lastId = $lastData['last_id'] ?? '';
-
-        if (empty($lastId)) {
-            return $prefix . '01';
-        } else {
-            $parts = explode('.', $lastId);
-            $lastNumber = (int)end($parts);
-            $newNumber = $lastNumber + 1;
-            return $prefix . str_pad($newNumber, 2, '0', STR_PAD_LEFT);
-        }
-    }
-
     /**
      * Simpan data lokasi (insert/update)
      */
@@ -97,16 +66,25 @@ class Lokasi extends MyController
             $d['parent_lokasi_id'] = null;
         }
 
-        $queryCheckUnique = "SELECT * FROM mst_lokasi WHERE lokasi_nm = '" . addslashes($d['lokasi_nm']) . "' AND tipe_lokasi = '" . addslashes($d['tipe_lokasi']) . "' AND deleted_st = 0";
+        $params = [
+            'lokasi_nm'   => $d['lokasi_nm'],
+            'tipe_lokasi' => $d['tipe_lokasi'],
+            'deleted_st'  => 0,
+        ];
+        $queryCheckUnique = "SELECT * FROM mst_lokasi WHERE lokasi_nm = :lokasi_nm AND tipe_lokasi = :tipe_lokasi AND deleted_st = :deleted_st";
+
         if (!empty($d['parent_lokasi_id'])) {
-            $queryCheckUnique .= " AND parent_lokasi_id = '" . addslashes($d['parent_lokasi_id']) . "'";
+            $queryCheckUnique .= " AND parent_lokasi_id = :parent_lokasi_id";
+            $params['parent_lokasi_id'] = $d['parent_lokasi_id'];
         } else {
             $queryCheckUnique .= " AND (parent_lokasi_id IS NULL OR parent_lokasi_id = '')";
         }
         if ($id != null) {
-            $queryCheckUnique .= " AND lokasi_id != '" . addslashes($id) . "'";
+            $queryCheckUnique .= " AND lokasi_id != :lokasi_id";
+            $params['lokasi_id'] = $id;
         }
-        $checkUnique = DbModel::rawData('row_array', $queryCheckUnique);
+
+        $checkUnique = DbModel::rawData('row_array', $queryCheckUnique, $params);
         if ($checkUnique != null) {
             return response()->json(_response('20', $this->uri, ['message' => 'Kombinasi Nama, Tipe, dan Lokasi Induk sudah ada!']));
         }
@@ -144,24 +122,7 @@ class Lokasi extends MyController
                 }
             }
         } catch (\Throwable $th) {
-            Log::error('Error saving location: ' . $th->getMessage(), ['trace' => $th->getTraceAsString()]);
             return response()->json(_response('10', $this->uri, ['message' => 'Terjadi kesalahan saat menyimpan data: ' . $th->getMessage()]));
-        }
-
-        if ($id) {
-            $old_id = $id;
-            $new_id = $d['lokasi_id'];
-            if ($old_id != $new_id) {
-                if (DbModel::validId('mst_lokasi', 'lokasi_id', $new_id)) {
-                    return response()->json(_response('20', $this->uri, ['message' => 'ID Lokasi baru sudah digunakan!']));
-                }
-            }
-            $result = DbModel::updateData('mst_lokasi', $d, ['lokasi_id' => $old_id]);
-            return response()->json(_response($result ? '02' : '12', $this->uri, $d));
-        } else {
-            $d['lokasi_id'] = $this->_generateLokasiId($d['tipe_lokasi'], $d['parent_lokasi_id']);
-            $result = DbModel::insertData('mst_lokasi', $d);
-            return response()->json(_response($result ? '01' : '11', $this->uri, $d));
         }
     }
 
@@ -195,3 +156,4 @@ class Lokasi extends MyController
         return LokasiModel::loadDatatables();
     }
 }
+
--- app/Modules/Master/Models/LokasiModel.php
+++ app/Modules/Master/Models/LokasiModel.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class LokasiModel extends Model
 {
-    protected static $nav_sess;
+    protected static $navSession;
 
     /**
      * Konstruktor model Lokasi, inisialisasi session navigasi.
@@ -23,8 +23,8 @@ class LokasiModel extends Model
      */
     protected static function initSession()
     {
-        if (is_null(self::$nav_sess)) {
-            self::$nav_sess = session(request('n'));
+        if (is_null(self::$navSession)) {
+            self::$navSession = session(request('n'));
         }
     }
 
@@ -38,27 +38,27 @@ class LokasiModel extends Model
     public static function generateLokasiId($tipe, $parentId = null)
     {
         $prefix = $parentId ? $parentId . '.' : '';
-        $query = "SELECT MAX(lokasi_id) as last_id FROM mst_lokasi WHERE tipe_lokasi = ? AND deleted_st = 0";
+        $sql = "SELECT MAX(lokasi_id) AS last_id FROM mst_lokasi WHERE tipe_lokasi = ? AND deleted_st = 0";
         $params = [$tipe];
 
         if ($parentId) {
-            $query .= " AND parent_lokasi_id = ?";
+            $sql .= " AND parent_lokasi_id = ?";
             $params[] = $parentId;
         } else {
-            $query .= " AND parent_lokasi_id IS NULL";
+            $sql .= " AND parent_lokasi_id IS NULL";
         }
 
-        $lastData = DbModel::rawData('row_array', $query, $params);
-        $lastId = $lastData['last_id'] ?? '';
+        $row = DbModel::rawData('row_array', $sql, $params);
+        $lastId = $row['last_id'] ?? '';
 
         if (empty($lastId)) {
             return $prefix . '01';
-        } else {
-            $parts = explode('.', $lastId);
-            $lastNumber = (int)end($parts);
-            $newNumber = $lastNumber + 1;
-            return $prefix . str_pad($newNumber, 2, '0', STR_PAD_LEFT);
         }
+
+        $parts = explode('.', $lastId);
+        $lastNumber = (int)end($parts);
+        $newNumber = $lastNumber + 1;
+        return $prefix . str_pad($newNumber, 2, '0', STR_PAD_LEFT);
     }
 
     /**
@@ -66,48 +66,58 @@ class LokasiModel extends Model
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    static function loadDatatables()
+    public static function loadDatatables()
     {
         self::initSession();
-        
-        $where = "1 = 1 ";
 
-        if (@self::$nav_sess['search']['data']['tipe_lokasi'] != '') {
-            $where .= " AND a.tipe_lokasi = '" . @self::$nav_sess['search']['data']['tipe_lokasi'] . "' ";
-        }
-        
-        if (@self::$nav_sess['search']['data']['parent_lokasi_id'] != '') {
-            $where .= " AND a.parent_lokasi_id = '" . @self::$nav_sess['search']['data']['parent_lokasi_id'] . "' ";
-        }
-        
-        if (@self::$nav_sess['search']['data']['active_st'] != '') {
-            $where .= " AND a.active_st = '" . @self::$nav_sess['search']['data']['active_st'] . "' ";
-        }
-        
-        if (@self::$nav_sess['search']['data']['term'] != '') {
-            $where .= " AND (
-                      LOWER(a.lokasi_id) LIKE '%" . @strtolower(self::$nav_sess['search']['data']['term']) . "%' 
-                      OR LOWER(a.lokasi_nm) LIKE '%" . @strtolower(self::$nav_sess['search']['data']['term']) . "%'
-                      OR LOWER(a.deskripsi) LIKE '%" . @strtolower(self::$nav_sess['search']['data']['term']) . "%'
-                      OR LOWER(b.lokasi_nm) LIKE '%" . @strtolower(self::$nav_sess['search']['data']['term']) . "%'
-                    ) ";
+        $filter = "1 = 1";
+
+        $searchData = self::$navSession['search']['data'] ?? [];
+
+        if (!empty($searchData['tipe_lokasi'])) {
+            $filter .= " AND a.tipe_lokasi = '" . addslashes($searchData['tipe_lokasi']) . "'";
         }
 
-        $query = "SELECT * FROM (
-                    SELECT
-                        a.lokasi_id, a.lokasi_nm, a.tipe_lokasi, a.parent_lokasi_id,
-                        a.deskripsi, a.active_st, a.denah_url,
-                        b.lokasi_nm as parent_lokasi_nm
-                    FROM mst_lokasi a
-                    LEFT JOIN mst_lokasi b ON a.parent_lokasi_id = b.lokasi_id
-                    WHERE $where AND a.deleted_st = 0
-                ) x ";
-                
-        $search = ['lokasi_id', 'lokasi_nm', 'tipe_lokasi', 'parent_lokasi_id', 'deskripsi'];
+        if (!empty($searchData['parent_lokasi_id'])) {
+            $filter .= " AND a.parent_lokasi_id = '" . addslashes($searchData['parent_lokasi_id']) . "'";
+        }
+
+        if (!empty($searchData['active_st'])) {
+            $filter .= " AND a.active_st = '" . addslashes($searchData['active_st']) . "'";
+        }
+
+        if (!empty($searchData['term'])) {
+            $term = strtolower($searchData['term']);
+            $filter .= " AND (
+                LOWER(a.lokasi_id) LIKE '%$term%' 
+                OR LOWER(a.lokasi_nm) LIKE '%$term%'
+                OR LOWER(a.deskripsi) LIKE '%$term%'
+                OR LOWER(b.lokasi_nm) LIKE '%$term%'
+            )";
+        }
+
+        $sql = "
+            SELECT * FROM (
+                SELECT
+                    a.lokasi_id,
+                    a.lokasi_nm,
+                    a.tipe_lokasi,
+                    a.parent_lokasi_id,
+                    a.deskripsi,
+                    a.active_st,
+                    a.denah_url,
+                    b.lokasi_nm AS parent_lokasi_nm
+                FROM mst_lokasi a
+                LEFT JOIN mst_lokasi b ON a.parent_lokasi_id = b.lokasi_id
+                WHERE $filter AND a.deleted_st = 0
+            ) x
+        ";
+
+        $searchColumns = ['lokasi_id', 'lokasi_nm', 'tipe_lokasi', 'parent_lokasi_id', 'deskripsi'];
         $where = null;
         $isWhere = null;
-        
-        $result = DbModel::datatablesQuery($query, $search, $where, $isWhere);
+
+        $result = DbModel::datatablesQuery($sql, $searchColumns, $where, $isWhere);
         return response()->json($result);
     }
 
